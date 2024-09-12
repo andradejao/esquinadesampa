ce0fe56787b705a6723267fc079936f920de3fb0
Camila Jodai <camijodai@gmail.com>
2024-09-12 15:30:35 -0300

    Update: finalizando a página detalhes e suas rotas.

--- src/views/js/restaurant.js
+++ src/views/js/restaurant.js
@@ -76,25 +76,23 @@ function carregarBusca() {
 }
 
 function carregarDetalhes() {
-    const idUrl = window.location.search.split("=")
-    console.log(idUrl)
-    const conteudo = document.querySelector('.conteudoDestaque')
-    const conteudoFeedback = document.querySelector('.cardFeedback')
+    const idUrl = window.location.search.split("=")[1];
+    const conteudo = document.querySelector('.conteudoDestaque');
+    const conteudoFeedback = document.querySelector('.cardFeedback');
 
-    fetch('http://10.26.45.33:4000/api/restaurante/detalhes/' + idUrl[1])
+    fetch('http://10.26.45.33:4000/api/restaurante/detalhes/' + idUrl)
         .then((response) => response.json())
         .then((data) => {
-            console.log(data)
+            console.log(data);
 
             data.payload.map((rs) => {
-
                 // Formatação da data de postagem do feedback
-                const data = rs.datapostagem
-                const dataObjeto = new Date(data)
-                const dia = String(dataObjeto.getUTCDate()).padStart(2, '0')
-                const mes = String(dataObjeto.getUTCMonth() + 1).padStart(2, '0')
-                const ano = dataObjeto.getUTCFullYear()
-                const dataFormatada = `${dia}-${mes}-${ano}`
+                const dataPostagem = rs.datapostagem;
+                const dataObjeto = new Date(dataPostagem);
+                const dia = String(dataObjeto.getUTCDate()).padStart(2, '0');
+                const mes = String(dataObjeto.getUTCMonth() + 1).padStart(2, '0');
+                const ano = dataObjeto.getUTCFullYear();
+                const dataFormatada = `${dia}-${mes}-${ano}`;
 
                 let card = `<section class="alinharDetalhes">
                     <h2 class="title">${rs.nomerestaurante}</h2>
@@ -110,13 +108,11 @@ function carregarDetalhes() {
                                 <img src="${rs.foto2}" class="d-block w-100" alt="..." id="imgCarrossel">
                             </div>
                         </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample"
-                            data-bs-slide="prev">
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                             <span class="visually-hidden">Previous</span>
                         </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample"
-                            data-bs-slide="next">
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
                             <span class="visually-hidden">Next</span>
                         </button>
@@ -125,65 +121,135 @@ function carregarDetalhes() {
                     <div class="card-body">
                         <p class="card-text">${rs.descricao}</p>
                     </div>
-            </section>
-            <section class="arrumarDetalhes">
-            <div class="card" style="width: 600px;">
-                <div class="card-body">
-                    <h3>Detalhes</h3>
-                    <p class="card-text">A partir de: R$${rs.faixadepreco}</p>
-                    <p class="card-text">Culinária ${rs.categoria}</p>
-                    <p class="card-text">Funcionamento: ${rs.horariofuncionamento}</p>
-                </div>
-            </div>
-            </section>
-            <section class="arrumarDetalhes">
-            <div class="card" style="width: 600px;">
-                <div class="card-body">
-                    <h3>Localização e contato</h3>
-                    <a href="mailto:${rs.emailcontato}" style="text-decoration: none; color: #000; cursor: pointer;"><img src="../public/img/mail.png" style="width: 20px; margin-right:10px;">${rs.emailcontato}</a>
-                    <br><br>
-                    <a href="https://api.whatsapp.com/send?phone=${rs.telefonecelular}" style="text-decoration: none; color: #000; cursor: pointer;">Telefone celular: ${rs.telefonecelular}</a>
-                    <br><br>
-                    <p class="card-text">Telefone residencial: ${rs.telefoneresidencial}</p>
-                    <a href="${rs.website}" style="text-decoration: none; color: #000;"><img src="../public/img/site.png" style="width: 20px; margin-right:10px;">${rs.website}</a>
-                    <br><br>
-                    <p class="card-text"><img src="../public/img/localization.png" style="width: 20px; margin-right:10px;">${rs.logradouro}, ${rs.numero}, ${rs.complemento}. ${rs.cep}</p>
-                    <p class="card-text"><img src="../public/img/home.png" style="width: 20px; margin-right:10px;">${rs.bairro}</p>
-                </div>
-            </div>
-            </section>`
+                </section>
+                <section class="arrumarDetalhes">
+                    <div class="card" style="width: 600px;">
+                        <div class="card-body">
+                            <h3>Detalhes</h3>
+                            <p class="card-text">A partir de: R$${rs.faixadepreco}</p>
+                            <p class="card-text">Culinária ${rs.categoria}</p>
+                            <p class="card-text">Funcionamento: ${rs.horariofuncionamento}</p>
+                        </div>
+                    </div>
+                </section>
+                <section class="arrumarDetalhes">
+                    <div class="card" style="width: 600px;">
+                        <div class="card-body">
+                            <h3>Localização e contato</h3>
+                            <a href="mailto:${rs.emailcontato}" style="text-decoration: none; color: #000; cursor: pointer;">
+                                <img src="../public/img/mail.png" style="width: 20px; margin-right:10px;">
+                                ${rs.emailcontato}
+                            </a>
+                            <br><br>
+                            <a href="https://api.whatsapp.com/send?phone=${rs.telefonecelular}" style="text-decoration: none; color: #000; cursor: pointer;">
+                                Telefone celular: ${rs.telefonecelular}
+                            </a>
+                            <br><br>
+                            <p class="card-text">Telefone residencial: ${rs.telefoneresidencial}</p>
+                            <a href="${rs.website}" style="text-decoration: none; color: #000;">
+                                <img src="../public/img/site.png" style="width: 20px; margin-right:10px;">
+                                ${rs.website}
+                            </a>
+                            <br><br>
+                            <p class="card-text">
+                                <img src="../public/img/localization.png" style="width: 20px; margin-right:10px;">
+                                ${rs.logradouro}, ${rs.numero}, ${rs.complemento}. ${rs.cep}
+                            </p>
+                            <p class="card-text">
+                                <img src="../public/img/home.png" style="width: 20px; margin-right:10px;">
+                                ${rs.bairro}
+                            </p>
+                        </div>
+                    </div>
+                </section>`;
 
-                conteudo.innerHTML = card
+                conteudo.innerHTML = card;
 
                 let cardFeedback = `
                 <section class="arrumarDetalhes">
-                <div class="card" style="width: 100%; padding: 1em; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
-                    <div class="card-body">
-                        <h5 class="card-title" style="font-weight: bold;">${rs.nome}</h5>
-                        <p class="card-subtitle text-muted" style="margin-bottom: 1em;">Postado em: ${dataFormatada}</p>
-                        <p class="card-text">${rs.opiniao}</p>
-                        <p class="card-text"> ${'⭐'.repeat(rs.nota)}</p>
+                    <div class="card" style="width: 100%; padding: 1em; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+                        <div class="card-body">
+                            <h5 class="card-title" style="font-weight: bold;">${rs.nome}</h5>
+                            <p class="card-subtitle text-muted" style="margin-bottom: 1em;">Postado em: ${dataFormatada}</p>
+                            <p class="card-text">${rs.opiniao}</p>
+                            <p class="card-text"> ${'⭐'.repeat(rs.nota)}</p>
+                        </div>
                     </div>
-                </div>
-            </section> `
+                </section>`;
 
-                conteudoFeedback.innerHTML += cardFeedback
-
-            })
-
+                conteudoFeedback.innerHTML += cardFeedback;
+            });
         })
+        .catch((error) => console.log(error));
 
-        .catch((error) => console.log(error))
+    // Chamar a função que carrega a média de feedback
+    carregarMediaFeedback();
+}
 
-        // Buscando rota de cálculo de média de nota
-    fetch('http://10.26.45.33:4000/api/feedback/listarmedia/' + idUrl[1])
+function carregarMediaFeedback() {
+    const idUrl = window.location.search.split("=")[1];
+    const extraDataDiv = document.querySelector('.extraData');
+
+    fetch('http://10.26.45.33:4000/api/feedback/listarmedia/' + idUrl)
         .then((response) => response.json())
         .then((data) => {
-            console.log(data)
+            console.log(data);
             data.payload.map((rs) => {
-                
-            })
-        })
+                let media = Number(rs.media);
+                let star = ""
+                let mediaHtml = ""
+                console.log(media.toFixed(1));
 
+                if (media.toFixed(0) == 1) {
+                    star = "⭐☆☆☆☆"
+                    mediaHtml = `
+                <br>
+                    <section class="mediaSection">
+                        <h3 class="mediaTitle">Média de Avaliações</h3>
+                        <p class="mediaContent">${star}</p>
+                    </section>`
+                } else if (media.toFixed(0) == 2) {
+                    star = "⭐⭐☆☆☆"
+                    mediaHtml = `
+                <br>
+                    <section class="mediaSection">
+                        <h3 class="mediaTitle">Média de Avaliações</h3>
+                        <p class="mediaContent">${star}</p>
+                    </section>`
+                } else if (media.toFixed(0) == 3) {
+                    star = "⭐⭐⭐☆☆"
+                    mediaHtml = `
+                <br>
+                    <section class="mediaSection">
+                        <h3 class="mediaTitle">Média de Avaliações</h3>
+                        <p class="mediaContent">${star}</p>
+                    </section>`
+                } else if (media.toFixed(0) == 4) {
+                    star = "⭐⭐⭐⭐☆"
+                    mediaHtml = `
+                <br>
+                    <section class="mediaSection">
+                        <h3 class="mediaTitle">Média de Avaliações</h3>
+                        <p class="mediaContent">${star}</p>
+                    </section>`
+                } else {
+                    star = "⭐⭐⭐⭐⭐"
+                    mediaHtml = `
+                <br>
+                    <section class="mediaSection">
+                        <h3 class="mediaTitle">Média de Avaliações</h3>
+                        <p class="mediaContent">${star}</p>
+                    </section>`
+                }
+
+
+                // Inserir a média de avaliações na div extraData
+
+
+                // Adicionando o conteúdo na div extraData
+                extraDataDiv.innerHTML = mediaHtml;
+            });
+        })
+        .catch((error) => console.log(error));
 }
 
