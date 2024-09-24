682d5ed74a3faf7a71440305aa2d2a22975e9a07
João Vitor Andrade <joaoandradeprime99@gmail.com>
2024-09-24 15:38:28 -0300

    Update: Atualizando script de criação do banco de dados

--- doc/ScriptDB.sql
+++ doc/ScriptDB.sql
@@ -1,70 +1,136 @@
-use dbesquinadesampa;
+CREATE DATABASE  IF NOT EXISTS `dbesquinadesampa`;
+USE `dbesquinadesampa`;
 
-create table restaurante (
-	idrestaurante int auto_increment primary key,
-    nomerestaurante varchar(50) not null,
-    categoria varchar(50) not null,
-    cnpj varchar(20) not null unique,
-    descricao text,
-    faixadepreco decimal(4,2) not null,
-    horariofuncionamento datetime not null,
-    idcontato int not null,
-    idendereco int not null,
-    idfoto int not null,
-    situacao int not null
-);
+--
+-- Table structure for table `restaurante`
+--
 
-create table contato (
-	idcontato int auto_increment primary key,
-    telefoneresidencial varchar(15),
-    emailcontato varchar(100),
-    telefonecelular varchar(15) not null,
-    website varchar(50)
-);
+DROP TABLE IF EXISTS `restaurante`;
 
-create table endereco (
-	idendereco int auto_increment primary key,
-    logradouro varchar(50) not null,
-	numero varchar(10) not null,
-    complemento varchar(50),
-    bairro varchar(30) not null,
-    cep varchar(10) not null
-    );
-    
-create table foto (
-	idfoto int auto_increment primary key,
-    fotocapa varchar(255) not null,
-    foto1 varchar(255),
-    foto2 varchar(255)
-);
+CREATE TABLE `restaurante` (
+  `idrestaurante` int NOT NULL AUTO_INCREMENT,
+  `nomerestaurante` varchar(50) NOT NULL,
+  `categoria` varchar(50) NOT NULL,
+  `cnpj` varchar(20) NOT NULL,
+  `descricao` text,
+  `faixadepreco` decimal(4,2) NOT NULL,
+  `horariofuncionamento` varchar(50) NOT NULL,
+  `datacadastro` datetime DEFAULT CURRENT_TIMESTAMP,
+  `idcontato` int NOT NULL,
+  `idendereco` int NOT NULL,
+  `idfoto` int NOT NULL,
+  `situacao` enum('ativo','inativo') NOT NULL,
+  PRIMARY KEY (`idrestaurante`),
+  UNIQUE KEY `cnpj` (`cnpj`),
+  KEY `fk.restaurante_pk.contato` (`idcontato`),
+  KEY `fk.restaurante_pk.endereco` (`idendereco`),
+  KEY `fk.restaurante_pk.foto` (`idfoto`),
+  CONSTRAINT `fk.restaurante_pk.contato` FOREIGN KEY (`idcontato`) REFERENCES `contato` (`idcontato`),
+  CONSTRAINT `fk.restaurante_pk.endereco` FOREIGN KEY (`idendereco`) REFERENCES `endereco` (`idendereco`),
+  CONSTRAINT `fk.restaurante_pk.foto` FOREIGN KEY (`idfoto`) REFERENCES `foto` (`idfoto`)
+) ENGINE=InnoDB AUTO_INCREMENT=94 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-create table login (
-	idlogin int auto_increment primary key,
-    email varchar(50) not null unique,
-    senha varchar(255) not null,
-    datacadastro datetime default current_timestamp()
-);
+-- /////////////////////////////////////////////
 
-create table feedback (
-	idfeedback int auto_increment primary key,
-    nome varchar(50) not null,
-    opiniao varchar(50) not null,
-    datapostagem datetime default current_timestamp(),
-    nota int not null,
-    idrestaurante int not null
-);
+CREATE DATABASE  IF NOT EXISTS `dbesquinadesampa`;
+USE `dbesquinadesampa`;
 
-alter table restaurante add constraint `fk.restaurante_pk.contato` foreign key restaurante(`idcontato`) 
-references contato(`idcontato`);
+--
+-- Table structure for table `foto`
+--
 
-alter table restaurante add constraint `fk.restaurante_pk.endereco` foreign key restaurante(`idendereco`) 
-references endereco(`idendereco`);
+DROP TABLE IF EXISTS `foto`;
 
-alter table restaurante add constraint `fk.restaurante_pk.foto` foreign key restaurante(`idfoto`) 
-references foto(`idfoto`);
+CREATE TABLE `foto` (
+  `idfoto` int NOT NULL AUTO_INCREMENT,
+  `fotocapa` varchar(255) NOT NULL,
+  `foto1` varchar(255) DEFAULT NULL,
+  `foto2` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`idfoto`)
+) ENGINE=InnoDB AUTO_INCREMENT=126 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-alter table feedback add constraint `fk.feedback_pk.restaurante` foreign key feedback(`idrestaurante`) 
-references restaurante(`idrestaurante`);
+-- /////////////////////////////////////////////
 
+CREATE DATABASE  IF NOT EXISTS `dbesquinadesampa`;
+USE `dbesquinadesampa`;
 
+--
+-- Table structure for table `login`
+--
 
+DROP TABLE IF EXISTS `login`;
+
+CREATE TABLE `login` (
+  `idlogin` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `datacadastro` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idlogin`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- /////////////////////////////////////////////
+
+CREATE DATABASE  IF NOT EXISTS `dbesquinadesampa`;
+USE `dbesquinadesampa`;
+
+--
+-- Table structure for table `contato`
+--
+
+DROP TABLE IF EXISTS `contato`;
+
+CREATE TABLE `contato` (
+  `idcontato` int NOT NULL AUTO_INCREMENT,
+  `telefoneresidencial` varchar(15) DEFAULT NULL,
+  `emailcontato` varchar(100) DEFAULT NULL,
+  `telefonecelular` varchar(15) NOT NULL,
+  `website` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`idcontato`)
+) ENGINE=InnoDB AUTO_INCREMENT=133 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- /////////////////////////////////////////////
+
+CREATE DATABASE  IF NOT EXISTS `dbesquinadesampa`;
+
+USE `dbesquinadesampa`;
+
+--
+-- Table structure for table `endereco`
+--
+
+DROP TABLE IF EXISTS `endereco`;
+
+CREATE TABLE `endereco` (
+  `idendereco` int NOT NULL AUTO_INCREMENT,
+  `logradouro` varchar(50) NOT NULL,
+  `numero` varchar(10) NOT NULL,
+  `complemento` varchar(50) DEFAULT NULL,
+  `bairro` varchar(30) NOT NULL,
+  `cep` varchar(10) NOT NULL,
+  PRIMARY KEY (`idendereco`)
+) ENGINE=InnoDB AUTO_INCREMENT=126 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- /////////////////////////////////////////////
+
+CREATE DATABASE  IF NOT EXISTS `dbesquinadesampa`;
+
+USE `dbesquinadesampa`;
+
+--
+-- Table structure for table `feedback`
+--
+
+DROP TABLE IF EXISTS `feedback`;
+
+CREATE TABLE `feedback` (
+  `idfeedback` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(30) NOT NULL,
+  `opiniao` varchar(100) NOT NULL,
+  `datapostagem` datetime DEFAULT CURRENT_TIMESTAMP,
+  `nota` int NOT NULL,
+  `idrestaurante` int NOT NULL,
+  PRIMARY KEY (`idfeedback`),
+  KEY `fk.feedback_pk.restaurante` (`idrestaurante`),
+  CONSTRAINT `fk.feedback_pk.restaurante` FOREIGN KEY (`idrestaurante`) REFERENCES `restaurante` (`idrestaurante`)
+) ENGINE=InnoDB AUTO_INCREMENT=64 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
